8117394954f2a208cc1e408b3a74e9bf16397851
Margge <marcelaguiza@gmail.com>
2012-03-19 12:56:42 -0500

    Transformed accordion to HAML

--- index.php
+++ index.php
@@ -10,23 +10,19 @@ $app->get('/', function() use ($app){
     $app->render('index.haml');
 });
 
-$app->get('/temp', function() use ($app){
-    require_once "templates/index.html";
-});
-
-$app->post('/calculate', function() use ($app){             
+$app->post('/calculate', function() use ($app){
 	// let's make sure we received the correct params
 	if($app->request()->post("individuals") === NULL){
 		die("No se enviaron individuos");
-	}                                                
-	
+	}
+
 	if($app->request()->post("formula") === NULL){
 		die("No se especific&oacute; la f&oacute;rmula");
 	}
-	
+
 	// separate the individuals (a comma-separated string)
 	$individuals = explode(",", $app->request()->post("individuals"));
-	
+
 	// get the formula calculator
 	$formula = $app->request()->post("formula");
 	if($formula == "simpson"){
@@ -38,7 +34,7 @@ $app->post('/calculate', function() use ($app){
 	} else {
 		die("Formula desconocida ($formula)");
 	}
-	
+
 	echo $calculator->calc();
 });
 
