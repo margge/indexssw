272f6f1d08448f1fecc67fb53950e5ed4fa81f95
Margge <marcelaguiza@gmail.com>
2012-03-19 14:17:53 -0500

    Added formulas calculation using AJAX

--- js/base.js
+++ js/base.js
@@ -12,4 +12,30 @@ $(document).ready(function() {
         rounded : true,
         enumerateSlides : true
     }).find('figcaption:first').show();
+
+    $('#add').click(function (){
+        var cant = $('.individual').size();
+        var add = "<div id=\"container_"+cant+"\"><input class=\"individual\" type=\"text\" id=\"individual_"+cant+"\"></input> <button id=\"remove_"+cant+"\">remover</button></div>";
+        $('#individual_set').append(add);
+        $("#remove_"+cant).click(function() {
+            $('#container_'+cant).remove();
+        });
+    });
+
+    $('#calculate').click(function(){
+        $("#loading").show();
+        var str = "";
+        var comma = "";
+        $('.individual').each(function(){
+            str += comma + $(this).val();
+            comma = ",";
+        });
+        var formula = $('input:radio[name=formula]:checked').val();
+        $.post('calculate', {individuals: str, formula: formula}, function(data) {
+            $('#result').html(data);
+            $("#loading").hide();
+        });
+    });
+
+    $("#loading").hide();
 });
