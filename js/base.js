c8fae6b10b2c6d95bbd8afeef4c90b2b35789238
Margge <marcelaguiza@gmail.com>
2012-03-19 12:24:32 -0500

    Refactored project structure

--- js/base.js
+++ js/base.js
@@ -1,1 +1,15 @@
-// foo bar baz
+$(document).ready(function() {
+    $('#ssw').liteAccordion({
+        onTriggerSlide : function() {
+            this.find('figcaption').fadeOut();
+        },
+        onSlideAnimComplete : function() {
+            this.find('figcaption').fadeIn();
+        },
+        autoPlay : false,
+        pauseOnHover : true,
+        theme : 'stitch',
+        rounded : true,
+        enumerateSlides : true
+    }).find('figcaption:first').show();
+});
